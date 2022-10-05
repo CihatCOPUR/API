b74667bba7768600220b5dc09e8c6008a2df1426
Cihat <copurcihat@gmail.com>
2022-10-05 11:50:42 +0300

    xd

--- app.js
+++ app.js
@@ -2,29 +2,40 @@ const div =document.querySelector(`div`);
 
 const input = document.querySelector(`input`)
 
+const btn=document.querySelector(`button`)
+
 const p= document.createElement(`p`);
 div.appendChild(p)
 
 ekle=async()=>{
-const name=input.value
-    const url=`https://api.tvmaze.com/search/shows?q=${name}`
+const isim=input.value
+    const Url=`https://api.tvmaze.com/search/shows?q=${isim}`
 
-const res= await fetch(url);
-
+const res= await fetch(Url);
+console.log(res);
+const data=await res.json();
+console.log(data);
+const {name,url,summary}=data[0];
 
 
     p.innerHTML=`<div class="card" style="width: 18rem;">
-    <img src="https://static.tvmaze.com/uploads/images/original_untouched/355/889469.jpg" class="card-img-top" alt="...">
+    <img src="${url}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">"Camdaki Kız"</h5>
-      <p class="card-text">Nalan is a beautiful young woman who earns everyone's love at first sight with her warmth. As the only child of the family enlarged hand baby, baby roses, lived in the existence of life and Nalan graduated with honors the best schools, Turkey finds's largest hotel chain, which Koroglu company as an architect at running than himself Sedat Koroglu' with marriage Eve. Although Nalan and Sedat began to walk hand in hand in this tale with hope, thinking that they are moving away from the dark secrets they have kept in their past; Soon their lives will sink into real darknes.</p>
+      <h5 class="card-title">"${name}"</h5>
+      <p class="card-text">${summary}</p>
       <a href="https://www.tvmaze.com/shows/54296/camdaki-kiz" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>`
 
 }
 
+btn.onclick=()=>{
 
-ekle()
+console.log(`clicked`);
+console.log(isim);
+  ekle()  ;
+}
+
+
 console.log(div);
 console.log(p);
